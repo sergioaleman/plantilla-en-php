e8c83f0873dded6856006bc4c92bfb2d21bff916
SERGIO ALEMAN <sergioaleman@yahoo.com>
2021-09-16 00:42:16 +0000

    Create share.php

---
+++ share.php
@@ -0,0 +1,4 @@
+<?php 
+echo '<h3>Síguenos en las redes sociales</h3><a href="http://www.facebook.com/"><img src="img/logo-facebook.jpg" width="96" height="96" alt="Síguenos en Facebook" title="Síguenos en Facebook"></a>
+<a href="https://twitter.com/"><img src="img/logo-twitter.jpg" width="96" height="96" alt="Síguenos en Twitter" title="Síguenos en Twitter"></a>';
+?>
